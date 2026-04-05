122500bc8c922df5e72dba8199fc2e4932c5e0c2
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-04-05 21:32:28 +0530

    scroll issue fixes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
-
+import ScrollToTop from "./components/ScrollToTop";
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
